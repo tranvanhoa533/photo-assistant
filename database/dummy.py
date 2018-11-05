92ee21ffa9c7f433c61e456747bf4fdf63c583c0
tranvanhoa533 <tranvanhoa533@gmail.com>
2018-11-05 20:03:00 +0700

    login

---
+++ database/dummy.py
@@ -0,0 +1,24 @@
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+ 
+engine = create_engine('sqlite:///ap.db', echo=True)
+ 
+# create a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+ 
+user = User("admin","admin")
+session.add(user)
+ 
+user = User("vanhoa","123")
+session.add(user)
+ 
+user = User("test","test")
+session.add(user)
+ 
+# commit the record the database
+session.commit()
+ 
+session.commit()
